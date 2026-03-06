3cabd0af006a15e58b9bade091f2ba74dfc54396
PabloHackWell <25011001011@hcesonepat.ac.in>
2026-03-06 22:54:39 +0530

    Add New Things

--- insert_data.py
+++ insert_data.py
@@ -1,11 +1,21 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # 1. Connect to your specific database
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
+# Connect securely
 db_connection = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Pablo$100", 
-  database="quiz_game"
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    database=db_name
 )
 cursor = db_connection.cursor()
 
